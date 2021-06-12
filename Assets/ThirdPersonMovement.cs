0282e99e2b299bf358b4443d38bc7c4a50c8f3e2
Joshua Corcoran <55488622+RaisinTheSteaks@users.noreply.github.com>
2021-06-12 17:26:18 +0100

    JC - Refactoring movement
    Updating the movement to use nav mesh and fix gravity on 3rd person controller

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -8,22 +8,44 @@ public class ThirdPersonMovement : MonoBehaviour
     public Transform cam;
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
+    public float gravityValue = -9.81f;
 
-    float turnSmoothVelocity;
+    private Vector3 fallVelocity = new Vector3(0f,0f,0f);
+    private bool groundedPlayer;
+    private float turnSmoothVelocity;
+    private float targetAngle;
+    private float angle;
+    private Vector3 moveDirection;
+
 
     void Update()
     {
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-        if(direction.magnitude>=0.1)
+
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && fallVelocity.y < 0)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            fallVelocity.y = 0f;
+        }
+
+
+
+        if (direction.magnitude>=0.1)
+        {
+            targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
         }
+        else
+        {
+            moveDirection = Vector3.zero;
+        }
+        fallVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(((moveDirection.normalized * speed) + fallVelocity )* Time.deltaTime); ;
     }
 }
